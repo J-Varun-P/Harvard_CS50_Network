789761ca1ba6d36bc170f08d8df43f9afea84521
Varun <varunpawarjetling@gmail.com>
2021-04-07 13:07:32 +0530

    first commit

---
+++ network/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
+    content = models.TextField()
+    likes = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name.username} posted on " + self.timestamp.strftime("%b %d %Y, %I:%M %p");
